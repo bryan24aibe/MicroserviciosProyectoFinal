811d1998ddcc8e77ddfc375704c0e6a6d7cbea37
Bryan Alejandro Lara Izurieta <153515590+bryan24aibe@users.noreply.github.com>
2025-01-26 19:31:10 -0500

    Update app.py

--- create_producto/app.py
+++ create_producto/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # CORS Configuration
 CORS(app, resources={r"/api/*": {"origins": "*"}})
-#Arreglo 4
+#Arreglo 5
 
 # Register the blueprint
 app.register_blueprint(product_bp)
