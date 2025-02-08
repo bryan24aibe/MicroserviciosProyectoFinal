aa64f11a17fa3f3ffa08862bc27f025d0e9f1340
balarai <b24ordonez@gmail.com>
2025-02-08 00:12:45 -0500

    feat: :rocket: test5

--- filtrado_busqueda/src/server.js
+++ filtrado_busqueda/src/server.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-
+//fix 1
 const server = new ApolloServer({
     typeDefs,
     resolvers,
