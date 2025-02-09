755872be57884b02b98323fe35e6ced5f1ad28bf
Jonablo <jonasanchez1993@gmail.com>
2025-02-08 19:06:51 -0500

    .

--- User_management/backend/listUsers/dbConnection.rb
+++ User_management/backend/listUsers/dbConnection.rb
@@ -14,7 +14,7 @@ def connect_to_db
       database: ENV['DB_NAME'],
       port: 3306
     )
-    puts "Connected to the database successfully jejex!"
+    puts "Connected to the database successfully"
     return client
   rescue Mysql2::Error => e
     # Handle connection error
