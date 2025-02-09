460075f82c8e7241df1d1cf5ba7d97d272f1ce9a
Jonablo <jonasanchez1993@gmail.com>
2025-02-09 01:09:03 -0500

    test

--- User_management/backend/PwdRecover/main.go
+++ User_management/backend/PwdRecover/main.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/joho/godotenv"
 )
 
 func enableCORS(next http.Handler) http.Handler {
@@ -15,23 +13,15 @@ func enableCORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		// Manejar preflight requests
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
-
 		next.ServeHTTP(w, r)
 	})
 }
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("❌ Error loading .env file")
-	}
-
 	db.InitRedis()
 	db.InitMySQL()
 
