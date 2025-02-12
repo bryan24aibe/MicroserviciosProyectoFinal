5b83c5b276aac3ddaa4ca28d36660cea1fc14473
balarai <b24ordonez@gmail.com>
2025-02-12 01:41:19 -0500

    feat(config): add NOTIFICATIONS_SERVICE_URL environment variable

--- Gestion_Producto/create_producto/config/config.py
+++ Gestion_Producto/create_producto/config/config.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from the .env file
+# Load environment variables
 load_dotenv()
 
 class Config:
     MONGODB_URI = os.getenv("MONGODB_URI")
     MONGODB_DB = os.getenv("MONGODB_DB")
+    NOTIFICATIONS_SERVICE_URL = os.getenv("NOTIFICATIONS_SERVICE_URL")  # Nueva variable
